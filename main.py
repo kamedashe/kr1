5c19b3bd547bc8dc297038749955291d062c5194
kamedashe <163843891+kamedashe@users.noreply.github.com>
2025-06-07 18:51:02 +0300

    Implement SQLAlchemy DAO and simple component service

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tkinter as tk
 from ui.main_window import MainWindow
 
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = MainWindow(root)
-    root.mainloop()
+    app = MainWindow()
+    app.mainloop()
